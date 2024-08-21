05feb6d135470b2480ff828c32bbbeb8028c926d
Mominul <islammominul3378@gmail.com>
2024-08-21 07:35:24 +0600

    added user home

--- src/Pages/Dashboard/UserHome/UserHome.jsx
+++ src/Pages/Dashboard/UserHome/UserHome.jsx
@@ -1,14 +1,46 @@
-import useAuth from "../../../Hooks/useAuth";
 
+import useAuth from "../../../Hooks/useAuth";
+import userImage from "../../../assets/user.png";
 
 const UserHome = () => {
-  const {user} = useAuth();
+  const { user } = useAuth();
+  const profilePicture = user?.photoURL || userImage;
+
   return (
-    <div>
-      <h1 className="text-3xl font-medium"><span>HI, Welcome </span></h1>
-      {
-        user?.displayName ? user.displayName: 'Back'
-      }
+    <div className="p-4">
+      <h1 className="text-3xl font-medium mb-8">
+        <span>HI, Welcome - </span>
+        <span className="font-bold bg-gradient-to-r from-[#2666f0] to-[#142753] bg-clip-text text-transparent">
+          {user?.displayName ? user.displayName : 'Back'}
+       </span>
+      </h1>
+      <div className="">
+        <div className="w-full h-60 bg-gray-200 rounded-lg overflow-hidden">
+          <img
+            src={user?.coverURL || "https://gajean.com/buster/wp-content/uploads/sites/26/elementor/thumbs/coupe-traveling-in-the-mountains-1-q0vp33sema7bbmnfh2mdf89wh0jtkgqboxhv3ac968.jpg"}
+            alt="Cover"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <img
+          src={profilePicture}
+          alt="User"
+          className="w-16 h-16 mt-8 rounded-full border-2 border-gray-300"
+        />
+        <div>
+          <h1 className="text-3xl py-2 font-semibold">{user?.displayName || 'Guest'}</h1>
+          <p className="text-lg text-gray-600">{user?.email || 'No email available'}</p>
+        </div>
+      </div>
+      <div className="mt-4">
+        <h2 className="text-xl font-semibold">About Me</h2>
+        <p>{user?.bio || 'No bio available'}</p>
+      </div>
+      <div className="mt-4">
+        <h2 className="text-xl font-semibold">More Details</h2>
+        <p>Birthdate: {user?.birthdate || 'Not provided'}</p>
+        <p>Address: {user?.address || 'Not provided'}</p>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Top3Tour/Top3Tour.jsx
+++ src/Pages/Home/Top3Tour/Top3Tour.jsx
@@ -1,8 +1,62 @@
 import StarRatings from "react-star-ratings";
+import useAuth from "../../../Hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import useCart from "../../../Hooks/useCart";
+import Swal from "sweetalert2";
 
 
 const Top3Tour = ({tour}) => {
-  const {image, rating, title, price, description} = tour;
+  const {image, rating, title, price, description, _id} = tour;
+  const {user} = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const axiosSecure = useAxiosSecure();
+  const [, refetch]= useCart();
+
+  const handleAddTourCart = () => {
+    if(user && user.email){
+      // send cart add database 
+      const cartItem = {
+        menuId: _id, 
+        email: user.email,
+        title,
+        image,
+        price, 
+      }
+      axiosSecure.post('/carts', cartItem)
+      .then(res => {
+        console.log(res.data)
+        if(res.data.insertedId){
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${title} add to your cart`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+          // reset cart update cart items 
+          refetch();
+        }
+      })
+    }
+    else{
+      Swal.fire({
+        title: "You are not Logged In",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, login"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // send the user to the login page
+          navigate('/login',{state: {from: location}})
+        }
+      });
+    }
+  }
   return (
     <div className="card  bg-base-100 md:w-96 shadow-xl mt-16">
   <figure className="h-52 overflow-hidden">
@@ -25,7 +79,7 @@ const Top3Tour = ({tour}) => {
     <p className="font-medium text-xl text-[#1a1e2c]">${price} <span className="text-slate-500">/Per person</span></p>
     <p className="text-lg font-medium text-slate-600">{description}</p>
     <div className="">
-      <button className="btn text-white bg-[#1b8ddd] hover:bg-[#1a1e2c]">See Details</button>
+      <button onClick={handleAddTourCart} className="btn text-white bg-[#1b8ddd] hover:bg-[#1a1e2c]">Add Tour</button>
     </div>
   </div>
 </div>
