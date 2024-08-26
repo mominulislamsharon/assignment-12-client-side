37928b40a7cae6742218afc5e596631d19c26c30
Mominul <islammominul3378@gmail.com>
2024-08-26 23:46:01 +0600

    api change

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -103,7 +103,7 @@ import ErrorPage from "../Pages/ErrorPage/ErrorPage";
         {
           path: "updateItem/:id",
           element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
-          loader: ({params}) => fetch(`http://localhost:5000/menu/${params.id}`)
+          loader: ({params}) => fetch(`https://assignment-tourist-guid-server.vercel.app/menu/${params.id}`)
         },
         {
           path: "allUsers",
